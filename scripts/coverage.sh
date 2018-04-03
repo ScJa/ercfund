bc52810c654a8d3c5f3ac324d2b4463bd3dd0210
Jakob Schneider <jakob.schneidr.ga@gmail.com>
2018-04-03 21:14:41 +0200

    Adapted testing scripts from open-zepplin and added solcover

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 SOLIDITY_COVERAGE=true scripts/test.sh
+exit 0
--- truffle.js
+++ truffle.js
@@ -4,12 +4,17 @@ require('babel-polyfill');
 module.exports = {
    networks: {
    development: {
-   host: "localhost",
-   port: 8545,
-   gas: 6712388,
-   gasPrice: 65000000000,
-   network_id: "*" // Match any network id
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+   },
+   coverage: {
+      host: 'localhost',
+      network_id: '*',
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+   },
   }
- }
 };
 
